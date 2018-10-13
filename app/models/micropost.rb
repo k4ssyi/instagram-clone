33bda8ab8f6b8598fe9d6775be63c5025b8e238c
かっしー <kasikasi00@gmail.com>
2018-10-13 14:24:36 +0900

    picture resize

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,6 +3,15 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true
+  validate  :picture_size
 
   mount_uploader :picture, ImgNameUploader
+
+  private
+    # アップロードされた画像のサイズをバリデーションする
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
